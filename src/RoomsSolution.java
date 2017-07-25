752ac628bd98d10dff4dc8589951d3fe256889f5
dokgo <tomasromeo1998@gmail.com>
2017-07-26 01:48:48 +0300

    Add input from file

--- src/RoomsSolution.java
+++ src/RoomsSolution.java
@@ -1,3 +1,6 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.Stack;
@@ -6,16 +9,15 @@ import java.util.Stack;
 public class RoomsSolution {
     private int numberOfNodes;
     private Stack<Integer> stack;
+    private static int INF = Integer.MAX_VALUE / 2;
 
 
     public RoomsSolution() {
-        stack = new Stack<Integer>();
+        stack = new Stack<>();
     }
 
 
-    public void tsp(int adjacencyMatrix[][])
-
-    {
+    public void tsp(int adjacencyMatrix[][]) {
 
         numberOfNodes = adjacencyMatrix[1].length - 1;
         int[] visited = new int[numberOfNodes + 1];
@@ -27,7 +29,7 @@ public class RoomsSolution {
         int min = Integer.MAX_VALUE;
 
         boolean minFlag = false;
-//        System.out.print(1 + "\t");
+        System.out.print(1 + "\t");
 
 
         while (!stack.isEmpty()) {
@@ -58,9 +60,7 @@ public class RoomsSolution {
 
             }
 
-            if (minFlag)
-
-            {
+            if (minFlag) {
 
                 visited[dst] = 1;
 
@@ -81,41 +81,59 @@ public class RoomsSolution {
     }
 
 
-    public static void main(String... arg) {
+    public static void main(String... arg) throws IOException {
 
         //TODO: Add proper input reader
 
         int number_of_nodes;
+        int number_of_vertices;
 
         Scanner scanner = null;
+        FileReader reader = null;
 
         try {
 
-            // Enter number of rooms
-            scanner = new Scanner(System.in);
+            reader = new FileReader("input");
+            // Enter number of roomsu
+            scanner = new Scanner(reader);
 
             number_of_nodes = scanner.nextInt();
+            number_of_vertices = scanner.nextInt();
 
-            int adjacency_matrix[][] = new int[number_of_nodes + 1][number_of_nodes + 1];
+
+            int distance_matrix[][] = new int[number_of_nodes + 1][number_of_nodes + 1];
+
+            for (int k = 1; k <= number_of_vertices; k++) {
+//                String[] vals = scanner.nextLine().split("\\s");
+
+
+                int i = scanner.nextInt(); // Integer.parseInt(vals[0]);
+                int j = scanner.nextInt(); // Integer.parseInt(vals[1]);
+                int weight = scanner.nextInt(); //Integer.parseInt(vals[2]);
+
+                distance_matrix[i][j] = weight;
+            }
 
             // Scan adjacency_matrix
             for (int i = 1; i <= number_of_nodes; i++) {
-
                 for (int j = 1; j <= number_of_nodes; j++) {
-
-                    adjacency_matrix[i][j] = scanner.nextInt();
-
+                    if (i != j && distance_matrix[i][j] == 0) {
+                        distance_matrix[i][j] = INF;
+                    }
+                    System.out.print(distance_matrix[i][j] + " ");
                 }
+                System.out.println();
 
             }
+
 
             for (int i = 1; i <= number_of_nodes; i++) {
 
                 for (int j = 1; j <= number_of_nodes; j++) {
 
-                    if (adjacency_matrix[i][j] == 1 && adjacency_matrix[j][i] == 0) {
+                    if ( distance_matrix[i][j] == 1 && distance_matrix[j][i] == 0) {
 
-                        adjacency_matrix[j][i] = 1;
+                        distance_matrix[j][i] = 1;
 
                     }
 
@@ -127,12 +145,17 @@ public class RoomsSolution {
 
             RoomsSolution tspNearestNeighbour = new RoomsSolution();
 
-            tspNearestNeighbour.tsp(adjacency_matrix);
+            tspNearestNeighbour.tsp(distance_matrix);
 
-        } catch (InputMismatchException inputMismatch) {
+        } catch (InputMismatchException | FileNotFoundException inputMismatch) {
 
             System.out.println("Wrong Input format");
 
+        } finally {
+            if (scanner != null)
+                scanner.close();
+            if (reader != null)
+                reader.close();
         }
 
         scanner.close();
